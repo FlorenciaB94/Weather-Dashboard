fef6fc80566775845a02fc622a8e6e647318a9f1
Flor C <florenciabelenc94@gmail.com>
2021-10-29 22:30:56 -0600

    Some API fetch calls... too many requests blocked account issue

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,35 @@
-const APIKey = "10a959efdd7a9844b3c1f8fa2c94a115"
+console.log(this);
+
+
+var APIKey = "10a959efdd7a9844b3c1f8fa2c94a115";
+var cityName = $('#city-name');
+var searchButton = $('#search');
+
+function getGeo(){
+    console.log("The button is working...")
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${APIKey}`)
+.then(function (response){
+    return response.json();
+})
+.then(function (data){
+    console.log(data);
+});
+};
+
+searchButton.on('click', getGeo());
+
+
+
+
+
+// function getWeather() {
+//     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${APIKey}`)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
+// };
+        
+//         getWeather();
